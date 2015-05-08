569aaf3e5cadac4e32c61d9e3db8aa96632fc672
PedroBlanco <pedro.blanco.wasmer@gmail.com>
2015-05-08 20:13:54 +0200

    Commit inicial Primer commit en local (fission), previo a la sincronización con github

---
+++ app-1/app/scripts/peg-init.js
@@ -0,0 +1,19 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+  var texto = document.getElementById('text1');
+    texto.innerText = '// Comentario
+::T1::
+Q1
+{A1}
+
+::T2::Q2a {A2}
+Q2b
+
+::T3::{A3}
+
+::T4::Q1
+
+
+';
+}, false);
