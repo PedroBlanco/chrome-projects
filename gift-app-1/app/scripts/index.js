4bf63e1412c1df69f3960d48f2b04631d7292ff0
PedroBlanco <pedro.blanco.wasmer@gmail.com>
2015-06-08 16:52:23 +0200

    Prueba con jquery en gift-app-1

--- gift-app-1/app/scripts/index.js
+++ gift-app-1/app/scripts/index.js
@@ -2,6 +2,10 @@
 document.addEventListener('DOMContentLoaded', function() {
   var h1 = document.getElementsByTagName('h1');
   if (h1.length > 0) {
-    h1[0].innerText = h1[0].innerText + ' \'Allo';
+    h1[0].innerText = h1[0].innerText + ' \'Allo!';
   }
+  
+  $('#contenedor').html('Prueba de jquery!');
+  
+  
 }, false);
